70b977065a38e42266f5f1159f2fb527a6cbed4d
Michael Xiang <yorkmike4@gmail.com>
2021-01-09 23:15:09 +0000

    seeds

---
+++ db/seeds/03_choices.sql
@@ -0,0 +1,8 @@
+INSERT INTO choices (poll_id, label, discription)
+VALUES (1, 'apple', 'red')
+       (1, 'orange', 'not red')
+       (1, 'banana', 'also not red')
+       (2, 'pizza', 'extra cheese')
+       (2, 'burger', 'Mcdonalds')
+       (2, 'fried chicken', 'KFC')
+       (2, 'Harry Potter', 'um...sure');
