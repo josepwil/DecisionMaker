0b048c644b591ff72afd5ce056ac5e00140038cf
Michael Xiang <yorkmike4@gmail.com>
2021-01-09 23:40:03 +0000

    added seeds

--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -1,5 +1,5 @@
 -- Widgets table seeds here (Example)
-INSERT INTO pools (creator_id, title, name_required, render_graph)
-VALUES (1, 'table1', TRUE, 'bar chart')
+INSERT INTO polls (creator_id, title, name_required, render_graph)
+VALUES (1, 'table1', TRUE, 'bar chart'),
       (1, 'table2', false, 'pie chart');
 
--- db/seeds/04_submissions.sql
+++ db/seeds/04_submissions.sql
@@ -1,26 +1,26 @@
 INSERT INTO submissions (poll_id, choice_id, rank, submitter_name)
-VALUES (1, 1, 1 , 'firstuser')
-       (1, 2, 2 , 'firstuser')
-       (1, 3, 3 , 'firstuser')
-       (1, 1, 2, 'userTwo')
-       (1, 2, 1, 'userTwo')
-       (1, 3, 3, 'userTwo')
-       (1, 1, 3, 'userThree')
-       (1, 2, 2, 'userThree')
-       (1, 3, 1, 'userThree')
-       (2, 4, 1, 'firstuser')
-       (2, 5, 2, 'firstuser')
-       (2, 6, 3, 'firstuser')
-       (2, 7, 4, 'firstuser')
-       (2, 4, 4, 'userTwo')
-       (2, 5, 3, 'userTwo')
-       (2, 6, 2, 'userTwo')
-       (2, 7, 1, 'userTwo')
-       (2, 4, 2, 'userThree')
-       (2, 5, 3, 'userThree')
-       (2, 6, 4, 'userThree')
-       (2, 7, 2, 'userThree')
-       (2, 4, 3, 'FOUR')
-       (2, 5, 2, 'FOUR')
-       (2, 6, 1, 'FOUR')
+VALUES (1, 1, 1 , 'firstuser'),
+       (1, 2, 2 , 'firstuser'),
+       (1, 3, 3 , 'firstuser'),
+       (1, 1, 2, 'userTwo'),
+       (1, 2, 1, 'userTwo'),
+       (1, 3, 3, 'userTwo'),
+       (1, 1, 3, 'userThree'),
+       (1, 2, 2, 'userThree'),
+       (1, 3, 1, 'userThree'),
+       (2, 4, 1, 'firstuser'),
+       (2, 5, 2, 'firstuser'),
+       (2, 6, 3, 'firstuser'),
+       (2, 7, 4, 'firstuser'),
+       (2, 4, 4, 'userTwo'),
+       (2, 5, 3, 'userTwo'),
+       (2, 6, 2, 'userTwo'),
+       (2, 7, 1, 'userTwo'),
+       (2, 4, 2, 'userThree'),
+       (2, 5, 3, 'userThree'),
+       (2, 6, 4, 'userThree'),
+       (2, 7, 2, 'userThree'),
+       (2, 4, 3, 'FOUR'),
+       (2, 5, 2, 'FOUR'),
+       (2, 6, 1, 'FOUR'),
        (2, 7, 4, 'FOUR');
