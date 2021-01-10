49bf4aed8d9d171ed26507dab032d2a976b1e879
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-10 18:46:52 +0000

    connecting vote router

--- routes/votes.js
+++ routes/votes.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router  = express.Router();
 
-const query = `
-SELECT title, option_name, description
-FROM polls
-JOIN
-`
 
+// access form to vote on a particular poll
 module.exports = (db) => {
-  router.get("/polls/:id/vote", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+  router.get("/:pollId", (req, res) => {
+    res.send('being hit');
+    console.log('req param is: ', req.params.pollId);
+    let query = `
+    SELECT title, option_name, description
+    FROM polls
+    JOIN options ON poll_id = polls.d
+    WHERE polls.id = ${req.params.poll_id};
+    `;
+    db.query(query)
       .then(data => {
         const users = data.rows;
+        console.log(data.rows)
         res.json({ users });
       })
       .catch(err => {
