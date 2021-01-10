e139126ba7766e3c823d3fac42002a7f0095f1ff
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-10 18:02:33 +0000

    db_fix_2

--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -1,5 +1,5 @@
 -- Widgets table seeds here (Example)
 INSERT INTO polls (creator_id, title, name_required, render_graph)
-VALUES (1, 'table1', TRUE, 'bar chart'),
-      (1, 'table2', false, 'pie chart');
+VALUES (1, 'Favourite Fruit', TRUE, 'bar chart'),
+      (1, 'Favourite Fast Food', false, 'pie chart');
 
---
+++ db/seeds/03_options.sql
@@ -0,0 +1,8 @@
+INSERT INTO options (poll_id, option_name, description)
+VALUES (1, 'apple', 'this is a red fruit'),
+       (1, 'orange', 'this fruit is orange'),
+       (1, 'banana', 'wow a yellow fruit'),
+       (2, 'pizza', 'extra cheese'),
+       (2, 'burger', 'Mcdonalds'),
+       (2, 'fried chicken', 'KFC'),
+       (2, 'taco bell', 'um...sure');
