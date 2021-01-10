cd4d614aa7fd81ab18410d08c125b20b86b4ce65
Michael Xiang <yorkmike4@gmail.com>
2021-01-10 18:11:07 +0000

    merging

--- db/schema/03_options.sql
+++ db/schema/03_options.sql
@@ -3,13 +3,6 @@ DROP TABLE IF EXISTS options CASCADE;
 CREATE TABLE options (
   id SERIAL PRIMARY KEY NOT NULL,
   poll_id INTEGER REFERENCES polls(id),
-<<<<<<< HEAD:db/schema/03_choices.sql
-  label VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  rank SMALLINT,
-  submission_id INTEGER REFERENCES submissions(id)
-=======
   option_name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL
->>>>>>> master:db/schema/03_options.sql
 );
--- db/seeds/04_submissions.sql
+++ db/seeds/04_submissions.sql
@@ -1,18 +1,7 @@
 INSERT INTO submissions (poll_id, submitter_name)
-<<<<<<< HEAD
-VALUES (1, 'submit1'),
-       (1, 'submit2'),
-       (1, 'submit3'),
-       (2, 'submit4'),
-       (2, 'submit5'),
-       (2, 'submit6'),
-       (2, 'submit7'),
-       (2, 'submit8');
-=======
 VALUES (1, 'Ash'),
       (1, 'Michael'),
       (1, 'Joe'),
       (2, 'Andy'),
       (2, 'Travis'),
       (2, 'Gary');
->>>>>>> master
