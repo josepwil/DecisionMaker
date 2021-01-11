00c8b637e9541f0d589147a9400a08841b96b64c
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-11 20:14:23 +0000

    enabling user to additional choice inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,14 +8,19 @@ $(document).ready(() => {
   //   }
   // });
 
+
+
   const createNewPollForm = function() {
     const $markup = $(`
       <h3>Add A New Poll</h3>
       <form>
         <label for="title">Poll Title:</label>
         <input type="text" id="title" name="title">
-        <label for="choice">Options:</label>
-        <input type="text" id="choice" name="choice">
+        <div class="options">
+          <label for="choice">Options:</label>
+          <input type="text" id="choice" name="choice">
+          <p class="addNewOption">Add</p>
+        </div>
         <label for="name_required">Voter Must Enter Name?</label>
         <input type="checkbox" id="name_required" name="name_required">
         <label for="render_as">Render Results as:</label>
@@ -30,15 +35,26 @@ $(document).ready(() => {
     return $markup;
   }
 
-  $.ajax({
-    method: "GET",
-    url: "/",
-    dataType: "html"
-  }).done((submissions) => {
-    const $pollForm = createNewPollForm();
-    $("body").append($pollForm);
+
+  $(".createPoll").on("click", function() {
+    $.ajax({
+      method: "GET",
+      url: "/",
+      dataType: "html"
+    }).done((submissions) => {
+      const $pollForm = createNewPollForm();
+      $("body").append($pollForm);
+    });
+  })
+
+
+
+
+  $(document).on('click','.addNewOption',function(){
+    const $newInput = `
+    <input type="text" id="choice" name="choice">
+    `;
+    $(".options").append($newInput);
   });
-
-
 
 });
