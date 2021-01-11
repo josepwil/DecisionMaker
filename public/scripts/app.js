977e5a5c445cf07e3ceff363cd8fa6f21807ae85
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-11 19:29:47 +0000

    rendering form on screen using ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,25 +1,44 @@
-$(() => {
+$(document).ready(() => {
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });
+
+  const createNewPollForm = function() {
+    const $markup = $(`
+      <h3>Add A New Poll</h3>
+      <form>
+        <label for="title">Poll Title:</label>
+        <input type="text" id="title" name="title">
+        <label for="choice">Options:</label>
+        <input type="text" id="choice" name="choice">
+        <label for="name_required">Voter Must Enter Name?</label>
+        <input type="checkbox" id="name_required" name="name_required">
+        <label for="render_as">Render Results as:</label>
+        <select name="render_as" id="render_as">
+          <option value="bar">Bar Chart</option>
+          <option value="Pie">Pie Chart</option>
+          <option value="Line">Line Chart</option>
+        </select>
+        <button>Create</button>
+      </form>
+    `)
+    return $markup;
+  }
+
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
+    url: "/",
+    dataType: "html"
+  }).done((submissions) => {
+    const $pollForm = createNewPollForm();
+    $("body").append($pollForm);
   });
 
-
-  // const createNewPollForm = function() {
-  //   const markup = $(`
-  //     <form>
-
-
-
-
-
-  //     </form>
-  //   `)
-  // }
 
 
 });
