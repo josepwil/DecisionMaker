3949e685ac978d04f89b94968bd86ffbada3a435
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-11 00:31:34 +0000

    finsing post route, inserting data into submissions and votes tables

--- routes/votes.js
+++ routes/votes.js
@@ -5,18 +5,16 @@ const router  = express.Router();
 // access form to vote on a particular poll
 module.exports = (db) => {
   router.get("/:poll_id", (req, res) => {
-    let query = `
-    SELECT title, option_name, description
+    const query = `
+    SELECT title, option_name, description, options.id AS option_id, polls.id AS poll_id
     FROM polls
     JOIN options ON poll_id = polls.id
-    WHERE polls.id = ${req.params.poll_id};
+    WHERE polls.id = $1;
     `;
-    db.query(query)
+    db.query(query, [req.params.poll_id])
       .then(data => {
         const pollData = data.rows;
         const templateVars = { pollData };
-        console.log(pollData);
-
         res.render("poll_vote", templateVars);
       })
       .catch(err => {
@@ -25,5 +23,36 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.post("/:poll_id", (req, res) => {
+    const pollId = req.params.poll_id;
+    const submitterName = req.body.submitter_name;
+    const rankArray = req.body.options_rank.split(',');
+
+  // insert submission into submissions table
+  const submissionsQuery = `
+  INSERT INTO submissions (poll_id, submitter_name)
+  VALUES ($1, $2)
+  RETURNING id;
+  `
+  const votesQuery = `
+  INSERT INTO votes (submission_id, option_id, rank)
+  VALUES ($1, $2, $3)
+  `
+  db.query(submissionsQuery, [pollId, submitterName])
+    .then(data => {
+      const submissionId = data.rows[0].id;
+      // using the submission id insert the votes
+      for (let i = 0; i < rankArray.length; i++) {
+        db.query(votesQuery, [submissionId, rankArray[i], i+1]);
+      }
+
+
+    });
+
+    console.log(submitterName);
+    console.log(rankArray);
+    res.send('being hit');
+  })
   return router;
 };
