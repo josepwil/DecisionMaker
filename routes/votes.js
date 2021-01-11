abdfdb84a21a520baf7ecdcf9bdd9168bebec491
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-11 00:53:06 +0000

    displaying confirmation of vote submission

--- routes/votes.js
+++ routes/votes.js
@@ -6,7 +6,7 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/:poll_id", (req, res) => {
     const query = `
-    SELECT title, option_name, description, options.id AS option_id, polls.id AS poll_id
+    SELECT title, option_name, description, options.id AS option_id, polls.id AS poll_id, name_required
     FROM polls
     JOIN options ON poll_id = polls.id
     WHERE polls.id = $1;
@@ -14,6 +14,7 @@ module.exports = (db) => {
     db.query(query, [req.params.poll_id])
       .then(data => {
         const pollData = data.rows;
+        console.log(pollData);
         const templateVars = { pollData };
         res.render("poll_vote", templateVars);
       })
@@ -29,30 +30,25 @@ module.exports = (db) => {
     const submitterName = req.body.submitter_name;
     const rankArray = req.body.options_rank.split(',');
 
-  // insert submission into submissions table
-  const submissionsQuery = `
-  INSERT INTO submissions (poll_id, submitter_name)
-  VALUES ($1, $2)
-  RETURNING id;
-  `
-  const votesQuery = `
-  INSERT INTO votes (submission_id, option_id, rank)
-  VALUES ($1, $2, $3)
-  `
-  db.query(submissionsQuery, [pollId, submitterName])
-    .then(data => {
-      const submissionId = data.rows[0].id;
-      // using the submission id insert the votes
-      for (let i = 0; i < rankArray.length; i++) {
-        db.query(votesQuery, [submissionId, rankArray[i], i+1]);
-      }
-
-
-    });
-
-    console.log(submitterName);
-    console.log(rankArray);
-    res.send('being hit');
-  })
-  return router;
+    // insert submission into submissions table
+    const submissionsQuery = `
+    INSERT INTO submissions (poll_id, submitter_name)
+    VALUES ($1, $2)
+    RETURNING id;
+    `
+    const votesQuery = `
+    INSERT INTO votes (submission_id, option_id, rank)
+    VALUES ($1, $2, $3)
+    `
+    db.query(submissionsQuery, [pollId, submitterName])
+      .then(data => {
+        const submissionId = data.rows[0].id;
+        // using the submission id insert the votes
+        for (let i = 0; i < rankArray.length; i++) {
+          db.query(votesQuery, [submissionId, rankArray[i], i+1]);
+        }
+        res.render('vote_cast');
+      });
+    })
+    return router;
 };
