587aba0fd13bcb0fb8b578698c1011fe4af68636
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-12 21:07:23 +0000

    showing modal with specific graph

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,31 @@ $(document).ready(() => {
   //   }
   // });
 
+  const createModal = function(content) {
+    const $markup = `
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>${content}</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+    return $markup;
+  }
+
   const renderGraph = function(obj) {
     console.log('~~~~ we make our graph here: ', obj)
     const $graph = `
@@ -25,7 +50,9 @@ $(document).ready(() => {
       let pollID = id[id.length - 1]
       const specificGraph = {[pollID]: pollObjs[pollID]}
       console.log('specific graph ~~~~', specificGraph);
-      renderGraph(specificGraph);
+      const newModal = createModal(specificGraph);
+      $(".content-container").append(newModal);
+      $("#exampleModalCenter").modal('show');
     }
   }
 
